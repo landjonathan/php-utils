e6a2a768dc88666633e1275f69a013f038239994
Jonathan Land <land.jonathan@gmail.com>
2021-01-13 16:18:40 +0200

    feat: var_log_group

--- log.php
+++ log.php
@@ -7,3 +7,16 @@ function var_log ($var) {
   $json = json_encode($var);
   echo "<script>console.log({$json})</script>";
 }
+
+/**
+ * Logs an array of values in a group to the Javascript console
+ * @param string $label
+ * @param array $items
+ */
+function var_log_group ($label = '', $items = []) {
+//  $trace = json_encode(debug_backtrace());
+  echo "<script>console.group('$label');";
+//  echo "console.log({'trace': $trace});";
+  foreach ($items as $key => $value) { $json = json_encode($value); echo "console.log('$key', $json);"; }
+  echo "console.groupEnd();</script>";
+}
