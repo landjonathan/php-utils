41bb171c9f61d45da94f686f8abcc88971652830
Jonathan Land <land.jonathan@gmail.com>
2021-06-27 15:04:27 +0300

    feat(cached-result): default parameters, log_to_console extracted

--- cached_result.php
+++ cached_result.php
@@ -1,12 +1,22 @@
 <?php
+
+require_once __DIR__ . '/log.php';
+
 /**
  * @param string $filename
  * @param int $expiration In seconds
  * @param callable $fetch_function A function that returns a string result (e.g. <code>return file_get_contents(...)</code>)
  * @param string $directory
+ * @param boolean $log_to_console
  * @return string Returns the result or an empty string
  */
-function cached_result ($filename, $expiration, $fetch_function, $directory = 'wp-content/uploads/cache') {
+
+function cached_result ($filename = 'cached-result.json', $expiration = 60, $fetch_function, $directory = 'wp-content/uploads/cache', $log_to_console) {
+  if (!isset($log_to_console)) {
+    /** @noinspection PhpTernaryExpressionCanBeReplacedWithConditionInspection */
+    $log_to_console = function_exists('is_prod') ? is_prod() : false;
+  }
+
   if (!is_dir($directory))
     mkdir($directory);
 
@@ -20,17 +30,17 @@ function cached_result ($filename, $expiration, $fetch_function, $directory = 'w
     $result = $fetch_function();
     file_put_contents($filepath, $result);
 
-    if (!is_prod()) {
+    if ($log_to_console) {
       $filetime_string = date('H:i:s', $filetime);
       $time_string = date('H:i:s', $time);
-      var_log("Busted {$filename} cache from {$filetime_string} UTC, and refreshed at {$time_string} UTC");
+      var_log("Busted $filename cache from $filetime_string UTC, and refreshed at $time_string UTC");
     }
   } else {
     $result = file_get_contents($filepath);
 
-    if (!is_prod()) {
+    if ($log_to_console) {
       $time_string = date('H:i:s', $filetime);
-      var_log("Serving cached {$filename} from {$time_string} UTC");
+      var_log("Serving cached $filename from $time_string UTC");
     }
   }
 
